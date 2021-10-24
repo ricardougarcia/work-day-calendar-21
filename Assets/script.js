8b4b25802f704b68d1317dd080a4fa8d5880b00a
Ricardo Garcia <ricardougarcia@gmail.com>
2021-10-24 12:31:13 -0400

    added additional psuedo code

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,13 @@
-// DEPENDENCIES
-
-// FUNCTIONS
-
-// DESIGN
-
-// USER INTERACTIONS
+// --------------------DEPENDENCIES--------------------
+//
+// --------------------FUNCTIONS--------------------
+// change color of boxes depending on time (past=grey, present=red, future=green)
+// store user text input to local storage
+// retrieve user input from local storage and display on screen
+// when user click button store user text input
+// bonus. when user clicks drop down they are presented with options to clear text content in user input field
+// --------------------DESIGN --------------------
+//
+// --------------------USER INTERACTIONS--------------------
+// when user clicks lock-in button they store their text input.
+// bonus. when user clicks drop down they are presented with options to clear text content in user input field
