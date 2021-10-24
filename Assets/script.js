dd98468512e840cef8fc80afb8fe4fdb7c8564db
Ricardo Garcia <ricardougarcia@gmail.com>
2021-10-24 12:41:39 -0400

    added id's to html and corresponding variables in script

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,8 @@
 // --------------------DEPENDENCIES--------------------
-//
+var schedulerBlock = document.getElementById("scheduler-block");
+var userTextInput0100 = document.getElementById("user-text-input-0100");
+var lockInButton = document.getElementById("lock-in-button");
+
 // --------------------FUNCTIONS--------------------
 // change color of boxes depending on time (past=grey, present=red, future=green)
 // store user text input to local storage
