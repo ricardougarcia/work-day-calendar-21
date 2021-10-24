ee8a82536e52b59186b533df51e4810145e17ff1
Ricardo Garcia <ricardougarcia@gmail.com>
2021-10-24 14:17:20 -0400

    added working color change function in js.

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,25 @@
 // --------------------DEPENDENCIES--------------------
-var schedulerBlock = document.getElementById("scheduler-block");
+var schedulerBlockClass = document.getElementsByClassName(
+  "scheduler-block-class"
+);
+var schedulerBlockID1 = document.getElementById("scheduler-block-1");
 var userTextInput0100 = document.getElementById("user-text-input-0100");
 var lockInButton = document.getElementById("lock-in-button");
+var currentTime = moment().format("hh");
+// console.log(currentTime); + "PM"
 
 // --------------------FUNCTIONS--------------------
 // change color of boxes depending on time (past=grey, present=red, future=green)
+// $("placeholder-button").click(function(){
+//     $("#user-text-input-0100").addClass("important blue");
+//   });
+function changeColor() {
+  if (currentTime === "02") {
+    $("#user-text-input-0100").addClass("present");
+  }
+}
+changeColor();
+
 // store user text input to local storage
 // retrieve user input from local storage and display on screen
 // when user click button store user text input
